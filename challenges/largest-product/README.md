cb6b9a91a8b85549fa98f74d78f8f3fd333d395f
brickfaced <brickfazed@hotmail.com>
2018-03-25 16:55:33 -0700

    Completed tests and README.md

---
+++ challenges/largest-product/README.md
@@ -0,0 +1,6 @@
+# Largest product of 2 adjacent values in a 2D array
+
+## Challenge
+Write a function called LargestProduct which takes in a 2D array. Without utilizing any of the built-in methods available to your language, return the largest product of 2 adjacent values within the 2D array.
+## Solution
+![](../../assets/largest_product.JPG "Largest_Product Whiteboard")
--- challenges/largest-product/largest_product.py
+++ challenges/largest-product/largest_product.py
@@ -2,12 +2,6 @@ def largest_product(two_d_list):
     current_largest = 0
     for i in range(len(two_d_list)):
         product = two_d_list[i][0] * two_d_list[i][1]
-        if current_largest < product:
+        if product > current_largest:
             current_largest = product
-    print(current_largest)
-
-
-test_list = [[10, 10], [3, 4], [5, 6], [7, 8]]
-
-
-largest_product(test_list)
+    return(current_largest)
