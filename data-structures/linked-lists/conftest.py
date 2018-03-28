28bf61f7842e55bfef70e7cb3b2b669d83ef3724
brickfaced <brickfazed@hotmail.com>
2018-03-27 23:28:48 -0700

    Troubleshooting the tests for linked lists

--- data-structures/linked-lists/conftest.py
+++ data-structures/linked-lists/conftest.py
@@ -2,3 +2,11 @@ import pytest
 from linked_list import LinkedList as LL
 
 
+@pytest.fixture
+def empty_ll():
+    return LL()
+
+
+@pytest.fixture
+def predefined_ll():
+    return LL([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
--- data-structures/linked-lists/linked_list.py
+++ data-structures/linked-lists/linked_list.py
@@ -3,20 +3,37 @@ from node import Node
 
 class LinkedList:
     def __init__(self, iter=[]):
+        """
+        Initializes a singly linked list
+        """
         self.head = None
         self._size = 0
 
     def __repr__(self):
+        """
+        Displays the head of the linked list
+        """
         return 'head => {}'.format(self.head.val)
 
     def __len__(self):
+        """
+        Displays the size of the linked list
+        """
         return self._size
 
     def insert(self, val):
+        """
+        Inserts a new value at the head of the linked list and increments the
+        size of the linked list
+        """
         self.head = Node(val, self.head)
         self._size += 1
 
     def find(self, val):
+        """
+        Takes in a value and searches for it inside the linked list and
+        returns True if it is found and False if not
+        """
         current = self.head
         while current:
             if val == current.val:
