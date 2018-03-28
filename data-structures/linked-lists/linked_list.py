de3be5ad8de192bc2f393391f5c5b523cf589109
brickfaced <brickfazed@hotmail.com>
2018-03-27 21:25:17 -0700

    Finished setting up linked_list file

--- data-structures/linked-lists/linked_list.py
+++ data-structures/linked-lists/linked_list.py
@@ -5,3 +5,21 @@ class LinkedList:
     def __init__(self, iter=[]):
         self.head = None
         self._size = 0
+
+    def __repr__(self):
+        return 'head => {}'.format(self.head.val)
+
+    def __len__(self):
+        return self._size
+
+    def insert(self, val):
+        self.head = Node(val, self.head)
+        self._size += 1
+
+    def find(self, val):
+        current = self.head
+        while current:
+            if val == current.val:
+                return True
+            current = current._next
+        return False
--- data-structures/linked-lists/ll_insertions.py
+++ data-structures/linked-lists/ll_insertions.py
@@ -1,8 +1,1 @@
-from node import Node
-
-
-class LinkedList:
-
-    def __init__(self, iter=[]):
-        self.head = None
-        self._size = 0
+from linked_list import LinkedList
