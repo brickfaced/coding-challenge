fdcdf0fb0137df05daed22f43656fd4b0e5143e5
brickfaced <brickfazed@hotmail.com>
2018-03-31 21:51:23 -0700

    Got it working for the most part, doesnt work for when ll_two has a greater size than ll_one

--- data-structures/linked-lists/ll_merge.py
+++ data-structures/linked-lists/ll_merge.py
@@ -5,7 +5,14 @@ from node import Node
 def merge_lists(ll_one, ll_two):
     current_one = ll_one.head
     current_two = ll_two.head
-    cache_one = current_one._next
-    cache_two = current_two._next
-    while current_one._next:
-     current_one._next = current_two
+
+    if len(ll_two) > len(ll_one):
+        current_one = ll_two.head
+        current_two = ll_one.head
+
+    while current_two is not None:
+        ll_one.insert_after(current_one.val, current_two.val)
+        current_one = current_one._next._next
+        current_two = current_two._next
+
+    return ll_one
