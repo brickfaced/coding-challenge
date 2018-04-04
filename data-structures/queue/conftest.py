a74706cbf6e49e74dbc74b8ff69273729dfa2b1e
brickfaced <brickfazed@hotmail.com>
2018-04-03 23:42:01 -0700

    Finished challenge and tests

--- data-structures/queue/conftest.py
+++ data-structures/queue/conftest.py
@@ -1,6 +1,6 @@
 import pytest
 from queue.queue import Queue as Q
-
+from queue.fifo_animal_shelter import AnimalShelter as AS
 
 @pytest.fixture
 def empty_q():
@@ -10,3 +10,13 @@ def empty_q():
 @pytest.fixture
 def predefined_q():
     return Q([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+
+
+@pytest.fixture
+def empty_as():
+    return AS()
+
+
+@pytest.fixture
+def predefined_as():
+    return AS(['cat', 'dog', 'cat', 'dog'])
