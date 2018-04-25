dc6d2bd1f7bdb8dc2261000fbd0953194adfa20b
brickfaced <brickfazed@hotmail.com>
2018-04-25 16:59:19 -0700

    Finished whiteboard challenge, tests, and updated README

--- data-structures/k_tree/README.md
+++ data-structures/k_tree/README.md
@@ -8,3 +8,13 @@ Write a function based on the specifications above, which takes in a k-ary tree.
 
 #### Solution
 ![](../../assets/plo.JPG)
+
+## Find all matches
+
+### Challenge
+- Write a function that takes a k-ary tree with non-unique values and a target value.
+- Return a collection of all the nodes from within the tree that match the provided value.
+- Avoid utilizing any of the built-in methods available to your language.
+
+#### Solution
+![](../../assets/find_matches.JPG)
--- data-structures/k_tree/k_tree_test.py
+++ data-structures/k_tree/k_tree_test.py
@@ -1,8 +1,11 @@
+"""Welcome to the KTree Testing Zone."""
 from .print_level_order import print_level_order as plo
+from .find_matches import find_matches as fm
 import pytest
 
 
 def test_ktree_insert(empty_kt):
+    """Tests to see if the insert method works as expected."""
     empty_kt.insert(1)
     assert empty_kt.root.val == 1
     empty_kt.insert(2, 1)
@@ -12,24 +15,41 @@ def test_ktree_insert(empty_kt):
 
 
 def test_ktree_pre_order(predefined_kt):
+    """Tests to see if the ktree is being traversed correctly."""
     order = []
     predefined_kt.pre_order(lambda n: order.append(n.val))
     assert order == [1, 2, 3, 4, 5]
 
 
 def test_ktree_post_order(predefined_kt):
+    """Tests to see if the ktree is being traversed correctly."""
     order = []
     predefined_kt.post_order(lambda n: order.append(n.val))
     assert order == [2, 4, 5, 3, 1]
 
 
 def test_ktree_breadth_first_order(predefined_kt):
+    """Tests to see if the ktree is being traversed correctly."""
     order = []
     predefined_kt.breadth_first_traversal(lambda n: order.append(n.val))
     assert order == [1, 2, 3, 4, 5]
 
 
 def test_print_level_order(predefined_kt, empty_kt):
+    """Tests to see if there is a newline in ever level of ktree."""
     assert plo(predefined_kt) == '1\n23\n45\n'
     with pytest.raises(ValueError):
         plo(empty_kt)
+
+
+def test_find_matches(non_unique_kt):
+    """Tests to see if all matches are found."""
+    assert [node.val for node in fm(non_unique_kt, 2)] == [2, 2, 2]
+    non_unique_kt.insert(2, 1)
+    assert [node.val for node in fm(non_unique_kt, 2)] == [2, 2, 2, 2]
+
+
+def test_find_no_matches(non_unique_kt):
+    """Tests to see if ValueError gets raised."""
+    with pytest.raises(ValueError):
+        fm(non_unique_kt, 24)
